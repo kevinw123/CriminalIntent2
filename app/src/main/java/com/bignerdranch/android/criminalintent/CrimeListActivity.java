be0f772a59e8ed113267df198eba0f5906d30d84
Kevin Wong <kevinwong_895@hotmail.com>
2016-08-14 12:51:14 -0700

    Adding two pane detail

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
+    protected int getLayoutResId(){
+        return R.layout.activity_twopane;
+    }
+
+    @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
     }
